32c59e7334d3706aac8c7ab44081932720dc89a3
Javier Sánchez Rois <jsanchez@gradiant.org>
2017-05-30 16:42:20 +0200

    Early return instead of switch

--- python/tennis.py
+++ python/tennis.py
@@ -17,12 +17,13 @@ class TennisGame1:
         result = ""
         temp_score = 0
         if self.p1points == self.p2points:
-            result = {
+            return {
                 0: "Love-All",
                 1: "Fifteen-All",
                 2: "Thirty-All",
             }.get(self.p1points, "Deuce")
-        elif (self.p1points >= 4 or self.p2points >= 4):
+
+        if (self.p1points >= 4 or self.p2points >= 4):
             minusResult = self.p1points - self.p2points
             if (minusResult == 1):
                 result = "Advantage " + self.player1Name
@@ -32,17 +33,18 @@ class TennisGame1:
                 result = "Win for " + self.player1Name
             else:
                 result = "Win for " + self.player2Name
-        else:
-            for i in range(1, 3):
-                if (i == 1):
-                    temp_score = self.p1points
-                else:
-                    result += "-"
-                    temp_score = self.p2points
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[temp_score]
+            return result
+
+        for i in range(1, 3):
+            if (i == 1):
+                temp_score = self.p1points
+            else:
+                result += "-"
+                temp_score = self.p2points
+            result += {
+                0: "Love",
+                1: "Fifteen",
+                2: "Thirty",
+                3: "Forty",
+            }[temp_score]
         return result
